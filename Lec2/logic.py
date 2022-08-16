397d9abf89b67717b1a4c6a71a16f77a3d085738
seasea128 <seasea@windowslive.com>
2022-08-16 14:24:48 +0700

    Add example

--- Lec2/logic.py
+++ Lec2/logic.py
@@ -1,8 +1,20 @@
 #! /usr/bin/python3
 
-salary = int(input("Enter salary: "))
-bonus =  int(input("Enter bonus: "))
+# salary = int(input("Enter salary: "))
+# bonus =  int(input("Enter bonus: "))
+# 
+# total_pay = salary + bonus
+# 
+# print(f"Your pay is {total_pay}")
 
-total_pay = salary + bonus
+total_seconds = float(input("Enter number of seconds: "))
 
-print(f"Your pay is {total_pay}")
+hours = total_seconds // 3600
+
+minutes = (total_seconds // 60) % 60
+
+seconds = total_seconds % 60
+
+print("Hours:", hours)
+print("Minutes:",minutes)
+print("Seconds:",seconds)
