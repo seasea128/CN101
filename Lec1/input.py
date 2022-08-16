c7e7142b6079ba67b436cc49d0567eb41a98d62a
seasea128 <seasea@windowslive.com>
2022-08-16 13:44:08 +0700

    [Init]

---
+++ Lec1/input.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Enter name
+first_name = input("Enter your name: ")
+
+# Enter last name
+last_name = input("Enter your last name: ")
+
+# Enter ID
+id = input("Enter your ID: ")
+
+# Enter majors
+major = input("Enter your majors: ")
+
+# Print first line
+print(f"Hello, my name is {first_name} {last_name}.")
+
+# Print second line
+print(f"My student ID is {id} and I'm studying in {major}.")
---
+++ Lec1/test.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# Print name
+print("Chanon Yothavut")
+# Print StudentID
+print("6410750076")
