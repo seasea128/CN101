d2d3eeed05833b5380c8836090134174fe0d3749
seasea128 <seasea@windowslive.com>
2022-08-23 16:16:40 +0700

    Change 80 to be a constant

--- Lec3/exercise.py
+++ Lec3/exercise.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+PASSING_GRADE = 80
+
 midterm = int(input("Enter midterm exam score: "))
 final = int(input("Enter final exam score: "))
 
@@ -7,5 +9,5 @@ total = midterm + final
 
 print(f"Your score is {total}")
 
-if total > 80:
+if total > PASSING_GRADE:
     print("You passed.")
