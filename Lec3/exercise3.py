5504d1dff26f826c0554d3c9cb0a5b329f75ea3a
seasea128 <seasea@windowslive.com>
2022-09-06 14:17:41 +0700

    Change how ./Lec3/exercise3.py is implemented

--- Lec3/exercise3.py
+++ Lec3/exercise3.py
@@ -4,6 +4,20 @@ int1 = int(input("Enter integer #1: "))
 int2 = int(input("Enter integer #2: "))
 int3 = int(input("Enter integer #3: "))
 
-intarr = [int1, int2, int3]
+maxVal = 0
 
-print(f"The maximum number is {max(intarr)}")
+if int1 > int2 and int1 > int3:
+    maxVal = int1
+elif int2 > int1 and int2 > int3:
+    maxVal = int2
+elif int3 > int1 and int3 > int2:
+    maxVal = int3
+else:
+    print("Something is wrong")
+
+print(f"The maximum number is {maxVal}")
+
+# Another, simpler impl
+
+# intArr = [int1, int2, int3]
+# print(f"The maximum number is {max(intArr)}")
