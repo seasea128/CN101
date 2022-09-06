dc0b561e8ceb285d58e63293a3507dbd8357e808
seasea128 <seasea@windowslive.com>
2022-09-06 16:09:26 +0700

    Make discount a separate variable and display it

--- Lec3/exercise4.py
+++ Lec3/exercise4.py
@@ -4,21 +4,25 @@ RETAIL_PRICE = 99
 
 quantity = int(input("Enter quantity: "))
 
-price: float = 0
+price = 0
+discount = 0
 
 if quantity < 10:
     price = quantity * RETAIL_PRICE
 elif quantity < 20:
     price = quantity * RETAIL_PRICE
-    price -= price * (10/100)
+    discount = price * (10 / 100)
 elif quantity < 50:
     price = quantity * RETAIL_PRICE
-    price -= price * (20/100)
+    discount = price * (20 / 100)
 elif quantity < 100:
     price = quantity * RETAIL_PRICE
-    price -= price * (30/100)
+    discount = price * (30 / 100)
 else:
     price = quantity * RETAIL_PRICE
-    price -= price * (40/100)
+    discount = price * (40 / 100)
 
-print(price)
+if discount > 0:
+    print(f"The discount is {discount:.2f}")
+
+print(f"The total price is {price - discount:.2f}")
