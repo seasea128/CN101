1207a8c2f47b6321e820c9e9f83e08ec3e416bb6
seasea128 <seasea@windowslive.com>
2022-11-08 17:21:01 +0700

    Add Lec11

--- Lec10/list.py
+++ Lec10/list.py
@@ -4,7 +4,8 @@ students = []
 
 choice = 10
 
-while choice != 0:
+
+def print_operation():
     print("What operation do you want to do")
     print("1: Append item to list.")
     print("2: Insert item to list.")
@@ -15,80 +16,241 @@ while choice != 0:
     print("7: Sort item in the list")  # Implemented
     print("8: Reverse list")  # Implemented
     print("9: Find min, max, sum, and average")
+    print("10: Compare between two values")
     print("0: Exit the program")
+
+
+def append_item():
+    id = int(input("Enter Student ID: "))
+    name = input("Enter name: ")
+    gpa = float(input("Enter GPA: "))
+    student = [id, name, gpa]
+    for row in students:
+        if row[0] == student[0]:
+            print(f"Duplicate Student ID at index {students.index(row)}")
+            return
+
+    students.append(student)
+    print("Append success")
+
+
+def insert_item():
+    id = int(input("Enter Student ID: "))
+    name = input("Enter name: ")
+    gpa = float(input("Enter GPA: "))
+    student = [id, name, gpa]
+    for row in students:
+        if row[0] == student[0]:
+            print(f"Duplicate Student ID at index {students.index(student)}")
+            return
+
+    index = int(input("Enter index: "))
+    students.insert(index, student)
+    print(f"Insert Student ID {student} at index {index} success")
+
+
+def search():
+    id = int(input("Enter Student ID: "))
+    for row in students:
+        if row[0] == id:
+            item = students.index(row)
+            print(f"Student ID: {id} is at index {item}")
+        else:
+            print(f"Student does not exist")
+
+
+def print_list():
+    selection = int(
+        input(
+            "Do you want to print all or print a portion of the list? (1 for print all, 2 for print portion): "
+        )
+    )
+    if selection == 1:
+        print(students)
+    elif selection == 2:
+        startRange = int(input("Enter the start of the range: "))
+        endRange = int(input("Enter the end of the range: "))
+        for x in range(startRange - 1, endRange):
+            print(students[x], end=", ")
+        print("")
+
+
+def remove_item_with_id():
+    id = int(input("Enter Student ID: "))
+    for row in students:
+        if row[0] == id:
+            students.remove(row)
+            print("Remove success")
+            return
+
+
+def remove_item_with_index():
+    index = int(input("Enter index: "))
+    if index > (len(students) - 1):
+        print("Index is larger than the size of the list")
+    else:
+        del students[index]
+        print("Remove success")
+
+
+def sort():
+    sort = int(input("Enter variable to sort by (1 = ID, 2 = Name, 3 = GPA): "))
+    if sort == 1:
+        sort_by_id()
+    elif sort == 2:
+        sort_by_name()  # Broken
+    elif sort == 3:
+        sort_by_gpa()  # Broken
+    else:
+        print("Selection invalid")
+
+
+def sort_by_gpa():
+    sort = int(
+        input("Enter direction of sorting (1 = Small to large, 2 = Large to small): ")
+    )
+    if sort == 1:
+        students.sort(key=lambda x: x[2])
+        print("Sorting success")
+    elif sort == 2:
+        students.sort(key=lambda x: x[2])
+        students.reverse()
+        print("Sorting success")
+    else:
+        print("Selection invalid")
+
+
+def sort_by_name():
+    sort = int(
+        input("Enter direction of sorting (1 = Small to large, 2 = Large to small): ")
+    )
+    if sort == 1:
+        students.sort(key=lambda x: x[1])
+        print("Sorting success")
+    elif sort == 2:
+        students.sort(key=lambda x: x[1])
+        students.reverse()
+        print("Sorting success")
+    else:
+        print("Selection invalid")
+
+
+def sort_by_id():
+    sort = int(
+        input("Enter direction of sorting (1 = Small to large, 2 = Large to small): ")
+    )
+    if sort == 1:
+        students.sort()
+        print("Sorting success")
+    elif sort == 2:
+        students.sort()
+        students.reverse()
+        print("Sorting success")
+    else:
+        print("Selection invalid")
+
+
+def reverse():
+    students.reverse()
+    print("Reverse success")
+
+
+def sum_min_max_gpa():
+    sum = 0
+    max = 0
+    min = 9999999999999999
+    for student in students:
+        if student[2] > max:
+            max = student[2]
+        if student[2] < min:
+            min = student[2]
+        sum += student[2]
+    print(f"Minimum value: {min}")
+    print(f"Maximum value: {max}")
+    print(f"Sum of value: {sum}")
+    print(f"Average of value: {sum / len(students)}")
+
+
+def compare():
+    choice = int(input("Enter variable to compare by (1 = ID, 2 = Name, 3 = GPA): "))
+    if choice == 1:
+        compare_by_id()
+    elif choice == 2:
+        compare_by_name()
+    elif choice == 3:
+        compare_by_gpa()
+
+
+def compare_by_id():
+    index1 = int(input("Enter index of first value to be compare: "))
+    if index1 > len(students):
+        print("Index is larger than the size of the list")
+    index2 = int(input("Enter index of second value to be compare: "))
+    if index2 > len(students):
+        print("Index is larger than the size of the list")
+    if students[index1][0] == students[index2][0]:
+        print(f"ID {students[index1][0]} is equal to ID {students[index2][0]}")
+    elif students[index1][0] > students[index2][0]:
+        print(f"ID {students[index1][0]} is greater than ID {students[index2][0]}")
+    elif students[index1][0] < students[index2][0]:
+        print(f"ID {students[index1][0]} is less than ID {students[index2][0]}")
+
+
+def compare_by_name():
+    index1 = int(input("Enter index of first value to be compare: "))
+    if index1 > len(students):
+        print("Index is larger than the size of the list")
+    index2 = int(input("Enter index of second value to be compare: "))
+    if index2 > len(students):
+        print("Index is larger than the size of the list")
+    if students[index1][1] == students[index2][1]:
+        print(f"Name {students[index1][1]} is equal to Name {students[index2][1]}")
+    elif students[index1][1] > students[index2][1]:
+        print(f"Name {students[index1][1]} is greater than Name {students[index2][1]}")
+    elif students[index1][1] < students[index2][1]:
+        print(f"Name {students[index1][1]} is less than Name {students[index2][1]}")
+
+
+def compare_by_gpa():
+    index1 = int(input("Enter index of first value to be compare: "))
+    if index1 > len(students):
+        print("Index is larger than the size of the list")
+    index2 = int(input("Enter index of second value to be compare: "))
+    if index2 > len(students):
+        print("Index is larger than the size of the list")
+    if students[index1][2] == students[index2][2]:
+        print(f"GPA {students[index1][2]} is equal to GPA {students[index2][2]}")
+    elif students[index1][2] > students[index2][2]:
+        print(f"GPA {students[index1][2]} is greater than GPA {students[index2][2]}")
+    elif students[index1][2] < students[index2][2]:
+        print(f"GPA {students[index1][2]} is less than GPA {students[index2][2]}")
+
+
+while choice != 0:
+    print_operation()
     choice = int(input("Enter operation: "))
     print(f"Your choice is {choice}")
 
     if choice == 1:
-        item = int(input("Enter Student ID: "))
-        if item in students:
-            print(f"Duplicate Student ID at index {students.index(item)}")
-        else:
-            students.append(item)
-            print("Append success")
+        append_item()
     elif choice == 2:
-        item = int(input("Enter Student ID: "))
-        if item in students:
-            print(f"Duplicate Student ID at index {students.index(item)}")
-        else:
-            index = int(input("Enter index: "))
-            students.insert(index, item)
-            print(f"Insert Student ID {item} at index {index} success")
+        insert_item()
     elif choice == 3:
-        search = int(input("Enter item: "))
-        item = students.index(search)
-        print(f"Student {search} is at index {item}")
+        search()
     elif choice == 4:
-        selection = int(
-            input(
-                "Do you want to print all or print a portion of the list? (1 for print all, 2 for print portion): "
-            )
-        )
-        if selection == 1:
-            print(students)
-        elif selection == 2:
-            startRange = int(input("Enter the start of the range: "))
-            endRange = int(input("Enter the end of the range: "))
-            for x in range(startRange - 1, endRange):
-                print(students[x], end=", ")
-            print("")
+        print_list()
     elif choice == 5:
-        item = int(input("Enter Student ID: "))
-        if item not in students:
-            print(f"Student ID {item} not exist in the list")
-        else:
-            students.remove(item)
-            print("Remove success")
+        remove_item_with_id()
     elif choice == 6:
-        index = int(input("Enter index: "))
-        if index > len(students):
-            print("Index is larger than the size of the list")
-        else:
-            del students[index]
-            print("Remove success")
+        remove_item_with_index()
     elif choice == 7:
-        sort = int(
-            input(
-                "Enter direction of sorting (1 = Small to large, 2 = Large to small): "
-            )
-        )
-        if sort == 1:
-            students.sort()
-            print("Sorting success")
-        elif sort == 2:
-            students.sort()
-            students.reverse()
-            print("Sorting success")
-        else:
-            print("Selection invalid")
+        sort()
     elif choice == 8:
-        students.reverse()
-        print("Reverse success")
+        reverse()
     elif choice == 9:
-        print(f"Minimum value: {min(students)}")
-        print(f"Maximum value: {max(students)}")
-        print(f"Sum of value: {sum(students)}")
-        print(f"Average of value: {sum(students)/ len(students)}")
+        sum_min_max_gpa()
+    elif choice == 10:
+        compare()
     elif choice == 0:
         print("Exiting")
         break
